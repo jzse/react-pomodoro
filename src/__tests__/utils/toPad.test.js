bd78017fb1b49b57d52c2b91c5fb33a6736bd502
jzse <jzse@users.noreply.github.com>
2017-04-15 16:44:54 +1000

    test: Try out jest

---
+++ src/__tests__/utils/toPad.test.js
@@ -0,0 +1,10 @@
+import toPad from '../../utils/toPad';
+
+describe('toPad', () => {
+  it('should pad numbers less than 10', () => {
+    expect(toPad(9)).toBe('09');
+  });
+  it('should not pad numbers greater than 10', () => {
+    expect(toPad(11)).toBe(11);
+  });
+});
