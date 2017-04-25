991d7f4dfc2bb8a7b24796b8c1fe13f4019b5d7f
jzse <jzse@users.noreply.github.com>
2017-04-25 16:56:50 +1000

    feat: Flash the separator to indicate the delay value to user

--- src/components/Clock/index.js
+++ src/components/Clock/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
-function Clock({ minutes, seconds, remaining }) {
-  // Flash separator inbetween every elapsed second.
-  const clockFlash = (remaining % 1000 !== 0) && true;
+function Clock({ minutes, seconds, remaining, delay }) {
+  // Flash the separator to indicate the delay value to user.
+  const clockFlash = ((remaining / delay) % 2);
   const classClockFlash = [
     'Clock__separator',
     (clockFlash ? '' : 'Clock__separator--isFlash'),
