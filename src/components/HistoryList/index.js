6a6555dcae14df3c2605a470d4138a11689fa622
jzse <jzse@users.noreply.github.com>
2017-05-01 16:28:28 +1000

    feat: Styled HistoryList

--- src/components/HistoryList/index.js
+++ src/components/HistoryList/index.js
@@ -4,20 +4,35 @@ import toPad from '../../utils/toPad';
 import './index.css';
 
 function HistoryList({ history }) {
+  const maxHistory = 11;
+  const recentHistory = (obj = history, n = maxHistory) =>
+    Object.keys(obj)
+      .sort((a, b) => a - b)
+      .reverse()
+      .slice(0, n)
+      .reduce((accumulator, currentValue, currentIndex) => {
+        accumulator[currentValue] = obj[currentValue];
+        accumulator[currentValue].displayIntensity = (maxHistory - currentIndex) / maxHistory;
+        return accumulator;
+      }, {});
   return (
     <ol reversed className="HistoryList">
-      {Object.values(history).reverse().map(item => <HistoryItem key={item.id} {...item} />)}
+      {Object.values(recentHistory())
+        .reverse()
+        .map(item => <HistoryItem key={item.id} {...item} />)}
     </ol>
   );
 }
 
-function HistoryItem({ name, initial, end }) {
-  const [minutes, seconds] = Object.values(extractTime(initial)).map(num =>
-    toPad(num),
-  );
+function HistoryItem({ name, initial, end, displayIntensity }) {
+  const [minutes, seconds] = Object.values(extractTime(initial)).map(num => toPad(num));
   const formatInitial = `${minutes}:${seconds}`;
+  const styleHistoryItem = {
+    opacity: displayIntensity,
+  };
+  const classNameHistoryItem = ['HistoryItem'].join(' ');
   return (
-    <li className="HistoryItem" title={formatInitial}>
+    <li style={styleHistoryItem} className={classNameHistoryItem} title={`${formatInitial} - ${end}`}>
       <span className="HistoryItem--detail">
         {end.toLocaleTimeString(navigator.language, { hour: '2-digit', minute: '2-digit' })}
       </span>
